fd6ff287692ac13681101e4be3acb87e6a27a462
Alexander Wilkinson <alexwilkinson@gmail.com>
2018-01-31 22:52:49 +0800

    added in basics for page views using PUG need to work on add in database support and form support next before creating user accounts

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-var express = require('express');
-var app = express();
+// package imports
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-var homeRoutes = require('./routes/home.routes')
+// my imports
+const homeRoutes = require('./routes/home.routes')
 
+/**
+ * Basic Logging middleware
+ * Middleware requires the next() callback
+ */
+app.use((req, res, next) =>{
+    console.log('A new request was received at: ' + Date.now() );
+    next();
+})
+
+// Parse URL encoded data
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Parse JSON data
+app.use(bodyParser.json());
+
+// enable loading of static files
+app.use(express.static(__dirname + '/public'));
+
+// Set Pug as the view engine to render the page
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+//Home Router
 app.use('/',homeRoutes);
 
 
 app.listen(3000);
+
+// app.use(express.static(__dirname + '/views'));
+// for prefix /static for all static files
+// app.use('/static', express.static('public'));
--- routes/home.routes.js
+++ routes/home.routes.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+/**
+ * Home Route
+ */
 router.get('/', (req, res) =>{
-    res.send('home route')
+    res.render('content', {
+        name: "My site",
+        url: "https://github.com"
+    });
 });
 
+/**
+ * Regex pattern ID
+ */
+router.get('/:id([0-9]{5})', (req, res)  =>{
+    res.send('Your Id:' + req.params.id);
+});
+
+/**
+ * Error Route
+ */
+router.get('**', (req, res) =>{
+    res.send("This is not the page you're looking for");
+});
 module.exports = router;
