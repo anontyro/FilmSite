1ef51c9b1deb0e4fa986e8726321e9ff1e47b005
Alexander Wilkinson <alexwilkinson@gmail.com>
2018-03-14 22:57:38 +0800

    deltas still no playing ball so just used the first part of the json object. Created the basic display for the news list will consider adding more later. also would need to paginate. next will look into the detail view

---
+++ public/js/shared/quill-display.js
@@ -0,0 +1,19 @@
+// const quill = new Quill('#quillContainer', {
+//     modules: {
+//         toolbar: [
+//             ['bold', 'italic'],
+//             ['link', 'blockquote', 'code-block', 'image'],
+//             [{ list: 'ordered' }, { list: 'bullet' }]
+//         ]
+//     },
+//     theme: 'snow'
+// });
+
+// const quillArr = []
+
+// const makeQuill = (containerName, delta) =>{
+//     const quill = new Quill('#'+containerName);
+//     quill.setContent(delta)
+//     quill.editor.disable();
+//     quillArr.push(quill);
+// }
